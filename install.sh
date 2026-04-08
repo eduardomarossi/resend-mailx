6f3457c45e2b777cc9b3839cc54aa7584ae38128
Eduardo Marossi <eduardo@MacBook-Pro-de-Eduardo.local>
2026-04-08 14:59:05 -0300

    fix

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+mv /usr/bin/mail /usr/bin/mail.old
+mv /usr/sbin/sendmail /usr/sbin/sendmail.old
 ln -s $(pwd)/mailx /usr/bin/mail
+ln -s $(pwd)/mailx /usr/sbin/sendmail
 chmod +x /usr/bin/mail
 chmod +x $(pwd)/mailx
